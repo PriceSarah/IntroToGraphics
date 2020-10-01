1c6aba8a70d950e285939dab5a3d15656bf3b9cb
Sarah <sfp1024@gmail.com>
2020-10-01 10:47:11 -0500

    added deltatime to update

--- GraphicsScene/GraphicsApp.h
+++ GraphicsScene/GraphicsApp.h
@@ -1,15 +1,34 @@
-#pragma once
+#pragma once  
+#include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
 #include <Gizmos.h>
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-#include <cstdio>
 
-using glm::vec3;
-using glm::vec4;
-using glm::mat4;
 class GraphicsApp
 {
+public:
+	GraphicsApp();
+	GraphicsApp(int width, int height, const char* title);
+	~GraphicsApp();
 
+	int run();
+
+	bool start();
+	bool update();
+	bool draw();
+	bool end();
+
+
+
+protected:
+	GLFWwindow* m_window;
+	glm::mat4 m_view;
+	glm::mat4 m_projection;
+
+private:
+	int m_width;
+	int m_height;
+	const char* m_title;
 };
 
